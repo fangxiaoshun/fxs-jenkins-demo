6249cb926854cfab2feeefcecfcf054e867c3022
方小顺 <fangxs@shuhaisc.com>
2023-05-02 18:16:20 +0800

    Initial commit

---
+++ src/main/java/com/fxs/jenkins/demo/FxsJenkinsDemoApplication.java
@@ -0,0 +1,13 @@
+package com.fxs.jenkins.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FxsJenkinsDemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FxsJenkinsDemoApplication.class, args);
+    }
+
+}
